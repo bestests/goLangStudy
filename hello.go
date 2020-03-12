977a760e53dcea1d4efa35a48905c350ce55ce2d
bestests <bestests@naver.com>
2020-03-12 04:01:11 +0000

    git repl test

--- hello.go
+++ hello.go
@@ -7,9 +7,6 @@ import (
 func main() {
 	fmt.Println("Hello World!!!")
 	fmt.Println("devlop test!!")
-<<<<<<< HEAD
-	fmt.Println("replDev test!!")
-=======
     fmt.Println("replDev test!!")
->>>>>>> 43dc52f3557d3c980a9001823cd55fab137eb0e9
+    fmt.Println("git repl test")
 }
