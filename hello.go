d770194c434abc93858a5a27e83992d43cc1bf22
bestests <bestests@naver.com>
2020-03-13 02:59:55 +0000

    20200313

--- hello.go
+++ hello.go
@@ -4,19 +4,26 @@ import (
 	"fmt"
 )
 
+func multiply (a, b int) (c int) {
+	c = a * b
+	return
+}
+
 func main() {
-	fmt.Println("Hello World!!!")
-	fmt.Println("devlop test!!")
-	fmt.Println("replDev test!!")
-	fmt.Println("git repl test")
-	fmt.Println("iPad Pro Test!!!")
-	fmt.Println("Tab Test!!!")
-	fmt.Println("test to work computer!!!")
-	fmt.Println("Mac vsc Test!!!")
-	fmt.Println("Devlop Test_1")
-	fmt.Println("Devlop Test_2")
-	fmt.Println("DevRepl Test_1")
-	fmt.Println("DevRepl iPad Test")
-	fmt.Println("devlop")
-	fmt.Println("devRepl")
+	const nameConst string = "Song"
+	fmt.Println(nameConst)
+
+	var nameVar string = "Song"
+
+	nameVar = "Chang"
+
+	fmt.Println(nameVar)
+
+	// Only use in function 
+	name := "Sup"
+	fmt.Println(name)
+
+	resultNum1 := multiply(2, 2)
+
+	fmt.Println(resultNum1)
 }
